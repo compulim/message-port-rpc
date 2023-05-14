97462c9fe094fc7b6f986e94d72173278b96e609
William Wong <compulim@hotmail.com>
2023-05-14 04:02:43 -0700

    Add test case

--- packages/message-port-rpc/src/messagePortRPC.spec.ts
+++ packages/message-port-rpc/src/messagePortRPC.spec.ts
@@ -328,3 +328,39 @@ describe('assymetric functions', () => {
     await expect(rpc2(123)).resolves.toBe('123');
   });
 });
+
+describe('throw synchronously', () => {
+  type Fn = () => never;
+  let fn: jest.Mock<Fn>;
+  let port1: MessagePort;
+  let port2: MessagePort;
+  let rpc: ReturnType<typeof messagePortRPC<Fn>>;
+  let promise: Promise<never>;
+
+  beforeEach(async () => {
+    ({ port1, port2 } = new MessageChannel());
+
+    fn = jest.fn<() => never>(() => {
+      throw new Error('Artificial.');
+    });
+
+    messagePortRPC<Fn>(port2, fn);
+    rpc = messagePortRPC<Fn>(port1);
+
+    promise = rpc();
+
+    // Catch it once so Node.js don't consider as unhandled rejection.
+    promise.catch(() => {
+      // Do nothing.
+    });
+
+    await waitFor(() => expect(fn).toBeCalledTimes(1));
+  });
+
+  afterEach(() => {
+    port1?.close();
+    port2?.close();
+  });
+
+  test('should reject', () => waitFor(() => expect(promise).rejects.toThrow('Artificial.')));
+});
