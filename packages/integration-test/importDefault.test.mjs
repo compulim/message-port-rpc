b18806643bf96f38d96a8070c99b119fd6067dd4
William Wong <compulim@users.noreply.github.com>
2023-09-10 18:59:50 -0700

    Add integration test (#22)

---
+++ packages/integration-test/importDefault.test.mjs
@@ -0,0 +1,22 @@
+import { messagePortRPC } from 'message-port-rpc';
+
+describe('simple scenario', () => {
+  let port1, port2;
+
+  beforeEach(() => {
+    ({ port1, port2 } = new MessageChannel());
+  });
+
+  afterEach(() => {
+    port1.close();
+    port2.close();
+  });
+
+  test('should work', async () => {
+    const clientStub = messagePortRPC(port1);
+
+    messagePortRPC(port2, (x, y) => Promise.resolve(x + y));
+
+    await expect(clientStub(1, 2)).resolves.toBe(3);
+  });
+});
